9a64af6457e73df009493330ee2e19eb45e165bf
Mark Brown <marksbrown@gmail.com>
2014-11-20 23:23:49 -0500

    Initial commit

---
+++ Assets/Scripts/FighterStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterStats : MonoBehaviour {
+	public int Health = 100;
+	public int Attack = 10;
+	public int Dodge = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
